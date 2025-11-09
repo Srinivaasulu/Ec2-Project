cb9a8dbfb65d96f8fae15476e2c2930877cad64a
Srinivaasulu <srinivaasulu456@gmail.com>
2025-11-09 22:56:24 +0530

    Configure AWS provider and EC2 instance
    Added AWS provider and EC2 instance resource configuration.

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,1 +1,13 @@
-
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+resource "aws_instance" "web" {
+    ami = "ami-0305d3d91b9f22e84"
+    instance_type = "t2.micro"
+    key_name = "kavi"
+    tags = {
+      name = "Web"
+    }
+  
+}
